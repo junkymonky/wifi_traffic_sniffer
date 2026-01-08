b0cb93efd78f852a95389a5e5de0dc017232d833
junkymonky <captaindivyapandey@gmail.com>
2026-01-09 02:49:38 +0530

    initialized project and finished sniffer.py

---
+++ sniffer.py
@@ -0,0 +1,24 @@
+from scapy.all import sniff,DNS, DNSQR,IP
+from datetime import datetime
+from database import save_traffic
+
+listeners = []
+
+def register_listener(callback):
+    listeners.append(callback)
+
+def notify(ip):
+    for cb in listeners:
+        cb(ip)
+
+def process_packet(packet):
+    if packet.haslayer(DNS) and packet.haslayer(DNSQR) and packet.haslayer(IP):
+        ip = packet[IP].src
+        domain = packet[DNSQR].qname.decode(errors='ignore').rstrip('.')
+        time = datetime.now().strftime("%H:%M:%S")
+
+        save_traffic(ip, domain, time)
+        notify(ip)
+
+def start_sniffing():
+    sniff(filter="udp port 53", prn=process_packet, store=False)
